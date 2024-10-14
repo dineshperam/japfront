6b1d56302edefcda827ec9340792ae6168761c2e
dineshperam <pdreddy972@gmail.com>
2024-10-15 00:20:11 +0530

    day one

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+app.use('/posts', postRoutes);
+
+
+const CONNECTION_URL = 'mongodb+srv://pdreddy972:NIPs8kd3n3tnGysq@cluster0.9ahscu3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL)
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
